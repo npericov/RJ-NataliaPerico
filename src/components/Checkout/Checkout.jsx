e599a89e2d92f64c6fead713dffe3c293e1466a9
Natalia Pericó <npericov@gmail.com>
2024-01-24 17:32:30 -0300

    form

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -3,7 +3,7 @@ import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 import { db } from "../../firebase/config";
 import { collection, addDoc } from "firebase/firestore";
-import Swal from "sweetalert2";
+
 
 const Checkout = () => {
     const { cart, totalCart, clearCart } = useContext(CartContext);
@@ -33,34 +33,35 @@ const Checkout = () => {
         fecha: new Date(),
     };
 
-    const ordersRef = collection(db, "orders");
+    const ordersRef = collection(db, 'orders');
 
     addDoc(ordersRef, orden)
-        .then((doc) => {
-        setOrderId(doc.id);
+        .then((doc) => {setOrderId(doc.id);
         clearCart();
-        Swal.fire("Gracias por tu compra!");
-        })
-        .catch((error) => {
-        console.error("Error al agregar el documento:", error);
         });
     };
 
+    if (orderId) {
+        return (
+            <div className="container mt-10 mb-20 max-w-screen-md mx-auto bg-white rounded-md shadow-lg hover:shadow-2xl overflow-visible p-4">
+                <h2 className="text-4xl font-semibold text-tecnica m-20 text-center">¡Gracias por tu compra!</h2>
+                <hr />
+                <p className=" font-semibold text-gray-500 p-5 text-center">Tu código de orden es: {orderId}</p>
+            </div>
+        );
+    }
+
+
     return (
-    <div className="container mt-10 max-w-screen-md mx-auto bg-white rounded-md shadow-lg hover:shadow-2xl overflow-visible p-4">
-        <h2 className="text-2xl font-semibold text-tecnica mb-4">Checkout</h2>
+    <div className="container mt-10 mb-20 w-1/3 mx-auto bg-white rounded-md shadow-lg hover:shadow-2xl overflow-visible p-4">
+        <h2 className="text-2xl font-semibold text-tecnica mb-4">Confirmación de tu compra</h2>
         <hr />
-        <h2>Ingresa tus datos para confirmar tu compra:</h2>
+        <h2 className="text-gray-500 p-2">Ingresa tus datos:</h2>
         <hr />
 
-        {orderId ? (
-        <div className="container m-auto mt-10">
-            <h2 className="text-4xl font-semibold text-gray-500">Gracias por tu compra</h2>
-            <hr />
-            <p>Tu código de orden es: {orderId}</p>
-        </div>
-        ) : (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-md mt-4">
+        <form onSubmit={handleSubmit} 
+        className="flex flex-col gap-4 max-w-screen mt-4 rounded-md">
+
             <input
             className="border p-2"
             type="text"
@@ -90,7 +91,6 @@ const Checkout = () => {
             Enviar
             </button>
         </form>
-    )}
     </div>
 );
 };
